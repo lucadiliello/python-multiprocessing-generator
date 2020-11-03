40e0254007d9e7bdcb6563ade1253f0327bf49c2
lucadiliello <luca.diliello@unitn.it>
2020-11-03 16:09:53 +0100

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='multiprocessing_python',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Integrate multiprocessing in a generator to speed up heavy computations',
+    author='Luca Di Liello',
+    license='GNUv2',
+)
